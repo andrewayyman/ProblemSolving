92fd43b49dc28d0518bb3f9459490b1a5d8f304c
Andrew Ayman <88544133+andrewayyman@users.noreply.github.com>
2024-06-04 18:03:59 +0300

    Time: 686 ms (100%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 SELECT E1.name
 FROM Employee E1 , Employee E2 
 Where E1.id = E2.managerId
-GROUP BY E1.id, E1.name
+GROUP BY E1.id , E1.name
 HAVING COUNT(E2.id) >= 5;
