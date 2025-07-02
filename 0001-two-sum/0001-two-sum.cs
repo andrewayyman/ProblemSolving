5151ac7481500114c25d00b6ea5515cc07451a95
Andrew Ayman <88544133+andrewayyman@users.noreply.github.com>
2025-07-02 21:37:10 +0300

    Time: 1 ms (98.5%), Space: 49.2 MB (30.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,12 +1,24 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        for(int i=0; i< (nums.Length-1); i++){
-            for(int j=i+1; j < nums.Length;j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[2] {i,j};
-                }
-            }
-        }
-        return new int[0];
+
+     Dictionary<int, int> ourDict = new Dictionary<int, int>();
+
+     for ( int i = 0; i < nums.Length; i++ )
+     {
+         int diff = target - nums[i];
+
+         if ( ourDict.ContainsKey(diff) )
+         {
+             return new int[] { ourDict[diff], i };
+         }
+
+         if ( !ourDict.ContainsKey(nums[i]) )
+         {
+             ourDict[nums[i]] = i;
+         }
+     
+ }
+
+ return new int[0];
     }
 }
